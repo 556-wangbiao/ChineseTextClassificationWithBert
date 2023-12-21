904534ee9214d9ec35a263f1792ef98bd9c180ab
wangbiao <18325764307@163.com>
2023-12-21 15:25:30 +0800

    Centos定时训练脚本

--- utils/run_in_env.sh
+++ utils/run_in_env.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+
+#往定时任务日志记录文件中加入时间记录
+echo "[$(date)] Task started" >> /mnt/inaisfs/data/wangbiao/ChineseTextClassificationBert/logs/scheduled_task.log
+
 # 进入项目目录
 cd "/mnt/inaisfs/data/wangbiao/ChineseTextClassificationBert"
 
 # 激活 CTC 虚拟环境
-source "/usr/local/anaconda3/bin/activate" CTC
+# source "/usr/local/anaconda3/bin/activate" CTC
+
+# eval "$(conda shell.bash hook)"
+source "/usr/local/anaconda3/etc/profile.d/conda.sh"
+# source "/mnt/inaisfs/data/wangbiao/.conda/envs/CTC/bin/activate"
+conda activate "/mnt/inaisfs/data/wangbiao/.conda/envs/CTC"
 
 # 从数据库获取数据并处理
 python utils/process_data.py
